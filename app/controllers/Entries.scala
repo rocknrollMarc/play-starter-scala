2322038e7ca5fc8fb851bdf4478fa4de3ce9c7eb
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-20 11:12:38 +0200

    fixed buggs

--- app/controllers/Entries.scala
+++ app/controllers/Entries.scala
@@ -1,27 +1,30 @@
-package package controllers
+/*
+ *
+ * Instant Play Framework Starter
+ * Phone Book Example
+ *   ______  ___                             ___                     ___ __
+ *  /   _  \/  /__  ______  ______  ______  /  /__  ______  ______  /  /  /
+ * /   ____/   _  \/   _  \/   _  \/   ___\/   _  \/   _  \/   _  \/      \
+ * \__/    \__/___/\______/\__/___/\______/\______/\______/\______/\__/___/
+ *
+ * Published under MIT License. Copyright (c) 2013 Daniel Dietrich, Packt Publishing
+ *
+ */
+package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.data.Forms._
+import models.{Entry, Entries => DAO}
 import play.api.data._
-
-// substitute class name !!!
-import models.{Entries => DAO}
-
-/** Uncomment the following lines as needed **/
-/**
-import play.api.Play.current
-import play.api.libs._
-import play.api.libs.iteratee._
-import play.api.libs.concurrent._
-import java.util.concurrent._
-import scala.concurrent.stm._
-import akka.util.duration._
-import play.api.cache._
-import play.api.libs.json._
-**/
+import play.api.data.Forms._
+import play.api.mvc._
 
 object Entries extends Controller {
+
+  val entryForm = Form(
+    mapping(
+      "name" -> text,
+      "phone" -> text,
+      "id" -> optional(longNumber)
+    )(Entry.apply)(Entry.unapply))
 
   def list(filter: String) = Action {
     val entries = DAO.findByName(filter.trim)
@@ -33,8 +36,24 @@ object Entries extends Controller {
     Redirect(routes.Entries.list())
   }
 
-  def edit(id: Long) = TODO
+  def add = Action {
+    val form = entryForm.fill(Entry())
+    Ok(views.html.edit(form, "Add entry"))
+  }
 
-  def save = TODO
+  def edit(id: Long) = Action {
+    DAO.findById(id) map { entry =>
+      val form = entryForm.fill(entry)
+      Ok(views.html.edit(form, "Edit entry"))
+    } getOrElse {
+      Redirect(routes.Entries.list())
+    }
+  }
+
+  def save = Action { implicit request =>
+    val entry = entryForm.bindFromRequest.get
+    DAO.save(entry)
+    Redirect(routes.Entries.list())
+  }
 
 }
--- app/models/Entries.scala
+++ app/models/Entries.scala
@@ -1,3 +1,15 @@
+/*
+ *
+ * Instant Play Framework Starter
+ * Phone Book Example
+ *   ______  ___                             ___                     ___ __
+ *  /   _  \/  /__  ______  ______  ______  /  /__  ______  ______  /  /  /
+ * /   ____/   _  \/   _  \/   _  \/   ___\/   _  \/   _  \/   _  \/      \
+ * \__/    \__/___/\______/\__/___/\______/\______/\______/\______/\__/___/
+ *
+ * Published under MIT License. Copyright (c) 2013 Daniel Dietrich, Packt Publishing
+ *
+ */
 package models
 
 import scala.collection.concurrent.TrieMap
@@ -7,21 +19,23 @@ case class Entry(name: String = "", phone: String = "", id: Option[Long] = None)
 
 object Entries {
 
-    val entries = TrieMap[Long, Entry]()
-    val uuid: AtomicLong = new AtomicLong()
+  val entries = TrieMap[Long, Entry]()
 
-    def delete(id: Long) = entries.remove(id)
+  val uuid: AtomicLong = new AtomicLong()
 
-    def findById(id: Long) = entries.get(id)
+  def delete(id: Long) = entries.remove(id)
 
-    def findByName(filter: String) = entries.values.filter {
-        _.name.toLowerCase.contains(filter.toLowerCase)
-    }.toSeq
+  def findById(id: Long) = entries.get(id)
 
-    def save(entry: Entry) =
+  def findByName(filter: String) = entries.values.filter {
+    _.name.toLowerCase.contains(filter.toLowerCase)
+  }.toSeq
+
+  def save(entry: Entry) =
     ( if (entry.id.isDefined) entry
-        else entry.copy(id = Some(uuid.incrementAndGet()))
-        ) match {
-        case e@Entry(_,_, Some(id)) => entries += (id -> e)
+      else entry.copy(id = Some(uuid.incrementAndGet()))
+    ) match {
+      case e@Entry(_, _, Some(id)) => entries += (id -> e)
     }
+
 }
